b9940fcc95fb9ad34493e8a50f2d45e0d7538b26
tewan aiamthet <tewan@sellsuki.com>
2022-02-15 14:54:56 +0700

    fix size page

--- src/components/YoutubeVideo.vue
+++ src/components/YoutubeVideo.vue
@@ -1,24 +1,41 @@
 <template>
 <div id="YoutubeVideo">
-    <div class="row mt-3 p-3">
-        <div class="col-12 d-xl-block">
-            <YouTube src="https://www.youtube.com/embed/DACglXR_hho" width="100%" height="540"/>
-            <!-- <iframe width="100%" height="540" src="https://www.youtube.com/embed/DACglXR_hho" title="YouTube video player" allowfullscreen></iframe> -->
+    <div class="row">
+        <div class="col-12 mt-3 d-none d-lg-block">
+            <iframe
+                width="100%" height="580"
+                src="https://www.youtube.com/embed/DACglXR_hho"
+                srcdoc="<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%;background-color: #000}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube.com/embed/DACglXR_hho><img src=https://s3.ap-southeast-1.amazonaws.com/staging.sellsuki.com-static/thumbnail_25a776fe36.webp alt='รวมพื้นที่โฆษณาบน LINE ที่เจ้าของธุรกิจควรรู้!'><span>▶</span></a>"
+                frameborder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+                title="รวมพื้นที่โฆษณาบน LINE ที่เจ้าของธุรกิจควรรู้!">
+            </iframe>
         </div>
-        <div class="col-12 d-block d-xl-none">
-            <YouTube src="https://www.youtube.com/embed/DACglXR_hho" width="100%" height="320"/>
-            <!-- <iframe width="100%" height="320" src="https://www.youtube.com/embed/DACglXR_hho" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
+        <div class="col-12 d-block d-lg-none" style="margin-top: -70px;">
+            <iframe
+                width="100%" height="320"
+                src="https://www.youtube.com/embed/DACglXR_hho"
+                srcdoc="<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%;background-color: #000}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube.com/embed/DACglXR_hho><img src=https://s3.ap-southeast-1.amazonaws.com/staging.sellsuki.com-static/thumbnail_25a776fe36.webp alt='รวมพื้นที่โฆษณาบน LINE ที่เจ้าของธุรกิจควรรู้!'><span>▶</span></a>"
+                frameborder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+                title="รวมพื้นที่โฆษณาบน LINE ที่เจ้าของธุรกิจควรรู้!">
+            </iframe>
         </div>
     </div>
 </div>
 </template>
 
 <script>
-import YouTube from 'vue3-youtube'
 
 export default {
     name: 'YoutubeVideo',
-    components: { YouTube },
+    data() {
+      return {
+        responseData:[],
+      }
+    }
 }
 </script>
 
