087712f67349218feb84e59ad8721431dba52a5a
tewan aiamthet <tewan@sellsuki.com>
2022-04-19 10:55:59 +0700

    test 180465

--- nuxt.config.js
+++ nuxt.config.js
@@ -120,7 +120,7 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
 
-  generate: {
-    fallback: '404.html'
-  }
+  // generate: {
+  //   fallback: '404.html'
+  // }
 }
